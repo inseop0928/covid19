17efbb4f3ef97bb9704ac51c2d69cadeca9e25cc
JenongInSeop <inseop0928@gmail.com>
2021-06-27 23:07:26 +0900

    select event add

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
-import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
 import Contents from './components/Contents';
-
 
 function App() {
   return (
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,27 @@
-import React from 'react'
+//import React from 'react'
+import React,{ useState, useEffect } from 'react'
 
 const Header = () => {
+    
+    //const [contentType,setContentType] = useState({});
+    //let [contentType,chgContentType]  = useState(['남자코트','강남 우동'])
+
+    const onContentType = (data) =>{
+
+        if(data.target.value === "Y"){
+            console.log("국내")
+
+        }else{
+            console.log("국외")
+        }
+    }
+
     return (
         <header className="header">
             <h1>COVID-19</h1>
-            <select>
-                <option>국내</option>
-                <option>세계</option>
+            <select onChange={onContentType}>
+                <option value = "Y">국내</option>
+                <option value = "N">세계</option>
             </select>
         </header>
     )
